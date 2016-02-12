0edd30b667a891ac63932a5495861295814cee5d
Céline Thiry <thiry.celi@gmail.com>
2016-02-12 14:15:33 +0100

    Pataugoire :)

--- js/script.js
+++ js/script.js
@@ -17,6 +17,41 @@ $(document).ready(function(){
 			});		
 	});
 
+	//Search lightbox multiselect
+	// $(".dropdown dt a").on('click', function() {
+	//   $(".dropdown dd ul").slideToggle('fast');
+	// });
+
+	// $(".dropdown dd ul li a").on('click', function() {
+	//   $(".dropdown dd ul").hide();
+	// });
+
+	// function getSelectedValue(id) {
+	//   return $("#" + id).find("dt a span.value").html();
+	// }
+
+	// $(document).bind('click', function(e) {
+	//   var $clicked = $(e.target);
+	//   if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+	// });
+
+	// $('.mutliSelect input[type="checkbox"]').on('click', function() {
+
+	//   var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+	//     title = $(this).val() + ",";
+
+	//   if ($(this).is(':checked')) {
+	//     var html = '<span title="' + title + '">' + title + '</span>';
+	//     $('.multiSel').append(html);
+	//     $(".hida").hide();
+	//   } else {
+	//     $('span[title="' + title + '"]').remove();
+	//     var ret = $(".hida");
+	//     $('.dropdown dt a').append(ret);
+
+	//   }
+	// });
+
 	// Animation of description profil panel
 	$('#base > header > section > div > section > header > h2').click(function(e){
 				e.preventDefault();
@@ -119,6 +154,7 @@ $(document).ready(function(){
 	// 			e.preventDefault();
 	// 			$('.lightbox').removeClass('active');							 
 	// });	
+
 
 
 });
